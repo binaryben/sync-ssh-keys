fed5ccbd710514a220bf721461e56f88deabadcc
Benny Michaels <12827556+binaryben@users.noreply.github.com>
2022-10-17 00:30:20 +0800

    🚧 Test usage for getting keys from GitHub

--- source/sync.nim
+++ source/sync.nim
@@ -1,5 +1,19 @@
+import
+  std/httpclient,
+  std/json,
+  strutils
+
 proc syncAuthorizedUsers* (
   args: seq[string],
 ): int =
-  echo "This is the sync command"
+  var client = newHttpClient()
+  let user = parseJson(client.getContent("http://api.github.com/users/binaryben"))
+  var username = to(user["login"], string)
+  username = replace(username, "\"", "")
+  echo("Public keys for @", username, " (", user["name"], ")\n")
+
+  let keys = parseJson(client.getContent("http://api.github.com/users/binaryben/keys"))
+  for i in countup(0, keys.len - 1):
+    echo(keys[i]["key"], "\n")
+
   return 1
