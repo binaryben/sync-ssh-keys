db9c24ab9be3e7494f26e8d7959a356af511cf1b
Benny Michaels <12827556+binaryben@users.noreply.github.com>
2022-10-17 14:51:13 +0800

    🚧 Able to download keys from GitHub
    Rate limit bypass with authentication via `ssh-keys config github.token`

---
+++ source/providers/git.nim
@@ -0,0 +1,58 @@
+import
+  std/httpclient,
+  std/json,
+  strutils
+
+import ../config
+
+proc downloadKeysFromGitUser * (
+  user: string,
+  config: string = getConfPath(),
+  provider: string = "github",
+  url: string = "",
+  endpoint: string = "",
+  token: string = "",
+  structure: string = "",
+): seq[string] =
+  ensureConfigExists(config)
+
+  var keys: seq[string] = @[]
+  var client = newHttpClient()
+
+  # Add known headers
+  if(provider == "github"):
+    client.headers = newHttpHeaders({ "Accept": "application/vnd.github+json" })
+  if(provider == "bitbucket"):
+    client.headers = newHttpHeaders({ "Accept": "application/json" })
+
+  # * Get API configuration settings
+  # ! This is slow (read file every user) and could be done
+  # ! in the syncAuthorizedUsers() proc.
+
+  var confURL = @[provider, "url"].join(".")
+  confURL = if (url == ""): getConf(config, confURL) else: url
+
+  var confEndpoint = @[provider, "endpoint"].join(".")
+  confEndpoint = if (endpoint == ""): getConf(config, confEndpoint) else: endpoint
+
+  var confToken = @[provider, "token"].join(".")
+  confToken = if (token == ""): getConf(config, confToken) else: token
+
+  if (confToken != ""):
+    let bearer = @["Bearer", confToken].join(" ")
+    client.headers = newHttpHeaders({ "Authorization": bearer })
+
+  # var confURL = @[provider, "url"].join(".")
+  # confURL = if (url == ""): getConf(config, confURL) else: url
+
+  # Generate API request
+  var api = @[confURL, confEndpoint].join("")
+  api = replace(api, "{{user}}", user)
+
+  let payload = parseJson(client.getContent(api))
+  # let payload = parseJson("[{\"key\": \"API calls suspended because of rate limiting\"}]")
+  for i in countup(0, payload.len - 1):
+    let key = to(payload[i]["key"], string)
+    add(keys, key)
+
+  return keys
--- source/sync.nim
+++ source/sync.nim
@@ -1,19 +1,64 @@
 import
-  std/httpclient,
-  std/json,
+  std/parsecfg,
+  std/terminal,
   strutils
+
+import
+  config,
+  providers/git
+
+type EventType = enum
+  SYNC_START,
+  SYNC_SUCCESS,
+  SYNC_FAILURE,
+
+proc notify (
+  event: EventType,
+  config: string
+): void =
+  var url: string
+  var message: string
+  let enabled: string = getConf(config, "health.enabled")
+
+  case event:
+    of SYNC_START:
+      url = getConf(config, "health.start")
+      message = @["Sending start notification to ", url, "\n"].join()
+    of SYNC_SUCCESS:
+      url = getConf(config, "health.success")
+      message = @["\n", "Sending success notification to ", url].join()
+    of SYNC_FAILURE:
+      url = getConf(config, "health.fail")
+      message = @["\n", "Sending failure notification to ", url].join()
+
+  if(enabled == "true"):
+    echo(message)
 
 proc syncAuthorizedUsers* (
   args: seq[string],
+  config: string = getConfPath(),
 ): int =
-  var client = newHttpClient()
-  let user = parseJson(client.getContent("http://api.github.com/users/binaryben"))
-  var username = to(user["login"], string)
-  username = replace(username, "\"", "")
-  echo("Public keys for @", username, " (", user["name"], ")\n")
+  ensureConfigExists(config)
+  notify(SYNC_START, config)
 
-  let keys = parseJson(client.getContent("http://api.github.com/users/binaryben/keys"))
-  for i in countup(0, keys.len - 1):
-    echo(keys[i]["key"], "\n")
+  var authorizedUsers = getConf(config, "path.users")
+  var output: seq[string] = @[]
 
-  return 1
+  for user in sections(loadConfig(authorizedUsers)):
+    stdout.styledWrite(styleDim, "Downloading keys for ", user, "...")
+    var provider = @[user, "provider"].join(".")
+    provider = getConf(authorizedUsers, provider)
+    let keys = downloadKeysFromGitUser(user, provider = provider)
+    stdout.eraseLine()
+
+    if(keys.len > 0):
+      for i in countup(0, keys.len - 1):
+        add(output, keys[i])
+      let count = intToStr(keys.len)
+      stdout.styledWriteLine(fgGreen, "✔", fgDefault, styleDim, "  Downloaded ", count, " keys for ", user)
+    else:
+      stdout.styledWriteLine(fgYellow, "⚠️  Could not load any keys for ", user)
+
+  notify(SYNC_SUCCESS, config)
+
+  return 0
