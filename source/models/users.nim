43c4c7a0a46aacb3e0112aead3c6d842977f1a20
Benny Michaels <12827556+binaryben@users.noreply.github.com>
2022-10-26 15:54:29 +0800

    ✨ getUser() working

--- source/models/users.nim
+++ source/models/users.nim
@@ -1,11 +1,16 @@
-import std/parsecfg
+import # Libraries
+  std/parsecfg
 
-import
-  ../models/conf
+import # Local
+  ../models/conf,
+  ../utils/logger
+
+let log = newLogger("models:users")
 
 # May be expanded later to source path from cli values
 proc getAuthorizedUserPath (): string =
   result = getConf("path.users")
+  log.debug("using locally configured authorized_users file")
 
 type
   UserStatus * = enum
@@ -20,23 +25,56 @@ type
     user*: string
     name*: string
     provider*: string
-    url*: string
-    endpoint*: string
+    api*: string
+    token*: string
+    meta*: string
+    keys*: string
+
+proc userExists * (user: string): bool =
+  log.debug("checking if " & user & " exists")
+  let file = getAuthorizedUserPath()
+  let authorizedUsers = loadConfig(file)
+
+  for authorizedUser in authorizedUsers.sections:
+    if authorizedUser == user:
+      log.debug(user & " found in " & file)
+      return true
+
+  log.debug(user & " NOT found in " & file)
+  return false
 
 proc getUser * (
-  user: string
+  user: string,
+  recursive: bool = true,
 ): User =
   let file = getAuthorizedUserPath()
   let authorizedUsers = loadConfig(file)
 
-  return User(
-    status: UserStatus.Exists,
+  result = User(
+    status: if userExists(user): UserStatus.Exists else: UserStatus.Ghost,
     user: user,
     name: authorizedUsers.getSectionValue(user, "name"),
     provider: authorizedUsers.getSectionValue(user, "provider"),
-    url: authorizedUsers.getSectionValue(user, "url"),
-    endpoint: authorizedUsers.getSectionValue(user, "endpoint"),
+    api: authorizedUsers.getSectionValue(user, "api"),
+    token: authorizedUsers.getSectionValue(user, "token"),
+    meta: authorizedUsers.getSectionValue(user, "meta"),
+    keys: authorizedUsers.getSectionValue(user, "keys"),
   )
+
+  if recursive and result.status == UserStatus.Exists:
+    result.api = if result.api == "":
+      getConf(result.provider & ".url")
+      else: result.api
+    result.token = if result.token == "":
+      getConf(result.provider & ".token")
+      else: result.token
+    result.token = if result.token == "false": "" else: result.token
+    result.meta = if result.meta == "":
+      getConf(result.provider & ".user")
+      else: result.meta
+    result.keys = if result.keys == "":
+      getConf(result.provider & ".keys")
+      else: result.keys
 
 proc saveUser * (
   user: User
@@ -53,15 +91,3 @@ proc removeUser * (
   authorizedUsers.delSection(user)
   authorizedUsers.writeConfig(file)
   return true
-
-
-# proc setConf * (
-#   user: string,
-#   value: string,
-# ): string =
-
-#   let splitKey = splitConfKey(key)
-#   var cfg = loadConfig(config)
-#   cfg.setSectionKey(splitKey.section, splitKey.subsection, value)
-#   cfg.writeConfig(config)
-#   return cfg.getSectionValue(splitKey.section, splitKey.subsection)
